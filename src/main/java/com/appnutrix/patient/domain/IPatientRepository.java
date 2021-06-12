3ddf9e01e64918d0a55595a3e7a28da24cfd7c61
zloop1995 <zloop1995@gmail.com>
2021-06-12 16:18:28 -0500

    First change
    first

--- src/main/java/com/appnutrix/patient/domain/IPatientRepository.java
+++ src/main/java/com/appnutrix/patient/domain/IPatientRepository.java
@@ -1,4 +1,15 @@
 package com.appnutrix.patient.domain;
 
-public interface IPatientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IPatientRepository extends JpaRepository<Patient, Integer>
+{
+    public Patient findByUsername (String username);
+    public List<Patient> findByLastName(String lastname);
+    public List<Patient> findByFirstNameAndLastName(String firstname, String lastname);
+    public List<Patient> findByFirstName(String firstName);
 }
