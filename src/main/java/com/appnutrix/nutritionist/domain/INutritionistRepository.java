3bcd32f99677d7188c0ea91d171dd1bb64743364
ThePain99 <67244047+ThePain99@users.noreply.github.com>
2021-06-13 19:19:43 -0500

    Nutricionist and Recommendation
    Classes and interfaces

--- src/main/java/com/appnutrix/nutritionist/domain/INutritionistRepository.java
+++ src/main/java/com/appnutrix/nutritionist/domain/INutritionistRepository.java
@@ -1,4 +1,15 @@
 package com.appnutrix.nutritionist.domain;
 
-public interface INutritionistRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface INutritionistRepository extends JpaRepository<Nutritionist, Integer> {
+    public Nutritionist findByUsername(String username);
+    public Nutritionist findByCnpNumber(Integer cnp_number);
+    public List<Nutritionist> findByFirstName(String firstname);
+    public List<Nutritionist> findByLastName(String lastname);
+    public List<Nutritionist> findByFirstNameAndLastName(String firstname, String lastname);
 }
--- src/main/java/com/appnutrix/nutritionist/domain/Nutritionist.java
+++ src/main/java/com/appnutrix/nutritionist/domain/Nutritionist.java
@@ -1,4 +1,54 @@
 package com.appnutrix.nutritionist.domain;
 
-public class Nutritionist {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//import com.appnutrix.professionalProfile.domain.ProfessionalProfile;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name="nutritionist")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Nutritionist implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    //@OneToOne(fetch = FetchType.LAZY)
+    //@OnDelete(action = OnDeleteAction.CASCADE)
+    //@JoinColumn(name="professional_profile", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    //private ProfessionalProfile professionalProfile;
+
+    @Column(name="username", nullable = false, length = 16)
+    private String username;
+
+    @Column(name="password", nullable = false, length = 50)
+    private String password;
+
+    @Column(name="first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name="last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(name="email", nullable = false, length = 50)
+    private String email;
+
+    @Column(name="cnp_number", nullable = false, length = 6)
+    private Integer cnpNumber;
+
+    @Column(name="created_at", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
 }
