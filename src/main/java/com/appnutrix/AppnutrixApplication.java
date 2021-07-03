f88d32e88e2726a5f07a44a6047b2532587eb905
Josue <josue.c1012@gmail.com>
2021-07-03 15:30:42 -0500

    singleton

--- src/main/java/com/appnutrix/AppnutrixApplication.java
+++ src/main/java/com/appnutrix/AppnutrixApplication.java
@@ -1,13 +1,13 @@
 package com.appnutrix;
 
+import com.appnutrix.Logger.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppnutrixApplication {
-
+    Logger logger = Logger.getInstance();
     public static void main(String[] args) {
         SpringApplication.run(AppnutrixApplication.class, args);
     }
-
 }
